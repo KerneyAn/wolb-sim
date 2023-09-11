0cae4bc544eb0e46836e83e9ee4de02c4bbecad4
Cade Mirchandani <cmirchan@ucsc.edu>
2023-09-10 18:28:19 -0700

    make position tuple

--- sim.py
+++ sim.py
@@ -9,23 +9,22 @@ class ColinearBlock:
     """
 
     genome1: str
-    genome1_pos: str
+    genome1_pos: tuple
     genome1_strand: str
     genome2: str
-    genome2_pos: str
+    genome2_pos: tuple
     genome2_strand: str
 
 
 def read_xmfa(xmfa_file: Path) -> list[ColinearBlock]:
     """
-    Reads xmfa entries only file and returns list of tuples of colinear blocks
+    Reads xmfa entries only file and returns list of colinear blocks
 
     Args:
         xmfa_file (Path): Path to xmfa colinear block entries only file
 
     Returns:
-        list[tuple[tuple]]: List of tuples of colinear blocks
-                        ex) [((wRi,1,100), (wmel,5,100))...]
+        list[ColinearBlock]
     """
     with open(xmfa_file, "r") as f:
         lines = f.readlines()
@@ -38,14 +37,21 @@ def read_xmfa(xmfa_file: Path) -> list[ColinearBlock]:
                 prev_two_lines = [
                     prev_line.strip().split() for prev_line in prev_two_lines
                 ]
+                genome1_pos = tuple(
+                    map(int, prev_two_lines[0][1].split(":")[1].split("-"))
+                )
+                genome2_pos = tuple(
+                    map(int, prev_two_lines[1][1].split(":")[1].split("-"))
+                )
                 block = ColinearBlock(
                     genome1="wmel",
-                    genome1_pos=prev_two_lines[0][1],
+                    genome1_pos=genome1_pos,
                     genome1_strand=prev_two_lines[0][2],
                     genome2="wri",
-                    genome2_pos=prev_two_lines[1][1],
+                    genome2_pos=genome2_pos,
                     genome2_strand=prev_two_lines[1][2],
                 )
+
                 out.append(block)
     return out
 
